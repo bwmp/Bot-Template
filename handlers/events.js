50108dd18b848b17c0aed7d0eb5e5ff514044568
AkiraDev <akira.devjs@gmail.com>
2023-01-20 10:55:02 -0800

    RAHHH SPELLING

--- handlers/events.js
+++ handlers/events.js
@@ -9,7 +9,7 @@ module.exports = client => {
 	readdirSync("./events/").forEach(file => {
 		const event = require(`../events/${file}`);
 		let eventName = file.split(".")[0];
-		client.logger.info(`Loading Events Client ${eventName}`);
+		client.logger.info(`Loading Client Event ${eventName}`);
 		client.on(eventName, event.bind(null, client));
 	});
 }; 
